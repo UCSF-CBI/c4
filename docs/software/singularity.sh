fcee84e845cc98f4dda0f0efd1cf1084345f3b4a
Henrik Bengtsson <hb@aroma-project.org>
2021-01-26 16:52:55 -0800

    tweaks

--- docs/software/singularity.sh
+++ docs/software/singularity.sh
@@ -23,6 +23,12 @@ fi
 
 1>&2 echo "WARNING: .mdi/singularity.sh.code-block.label=run needs to be updated manually"
 
+1>&2 echo "WARNING: .mdi/singularity.sh.code-block.label=shell needs to be updated manually"
+
+1>&2 echo "WARNING: .mdi/singularity.sh.code-block.label=shell-nobind needs to be updated manually"
+
+1>&2 echo "WARNING: .mdi/singularity.sh.code-block.label=shell-bind needs to be updated manually"
+
 mdi_code_block --label="rscript-sum" --workdir=lxc <<EOF
 singularity exec rocker_r-base.img Rscript -e "sum(1:10)"
 
